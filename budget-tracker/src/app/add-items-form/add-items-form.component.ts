d0d92a53655d0f4b67b2dd8c0513fec064c7db5e
LouisLouis19 <andreaslouis.louis@gmail.com>
2021-07-29 17:15:44 +0800

    Add items functionality, with item description and amount

---
+++ budget-tracker/src/app/add-items-form/add-items-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-items-form',
+  templateUrl: './add-items-form.component.html',
+  styleUrls: ['./add-items-form.component.scss']
+})
+export class AddItemsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- budget-tracker/src/app/app.module.ts
+++ budget-tracker/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AddItemsFormComponent } from './add-items-form/add-items-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AddItemsFormComponent
   ],
   imports: [
     BrowserModule,
